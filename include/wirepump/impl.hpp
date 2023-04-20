0451b903ab32b6c1b7b2383570d907719f2a43d8
Jorge Prendes <jorge.prendes@gmail.com>
2023-04-20 16:31:49 +0100

    Fix missing "const"

--- include/wirepump/impl.hpp
+++ include/wirepump/impl.hpp
@@ -29,7 +29,7 @@ struct read_impl {
 
 template <typename Stream, typename T>
 struct write_impl {
-    static auto write(Stream & stream, T & v) -> coroutines::sync_task<void> {
+    static auto write(Stream & stream, T const & v) -> coroutines::sync_task<void> {
         if constexpr (std::same_as<uint8_t, T>) {
             static_assert(traits::always_false_v<Stream>, "Unimplemented adapter to write to 'Stream'.");
         } else {
@@ -40,10 +40,10 @@ struct write_impl {
 };
 
 template <typename Stream>
-using read_result = decltype(wirepump::read_impl<Stream, uint8_t>::read(std::declval<Stream &>(), std::declval<uint8_t &>()));
+using read_result = decltype(read_impl<Stream, uint8_t>::read(std::declval<Stream &>(), std::declval<uint8_t &>()));
 
 template <typename Stream>
-using write_result = decltype(wirepump::write_impl<Stream, uint8_t>::write(std::declval<Stream &>(), std::declval<uint8_t const &>()));
+using write_result = decltype(write_impl<Stream, uint8_t>::write(std::declval<Stream &>(), std::declval<uint8_t const &>()));
 
 template <typename Stream, typename T>
 auto read(Stream & c, T & v) {
